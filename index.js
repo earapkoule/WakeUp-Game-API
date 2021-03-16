aeccffad43f66d07eadb3d7c7c91ed67ac21295e
Eirini Arapkoule <eirini.arapkoule@gmail.com>
2021-03-16 09:43:27 +0200

    Add get, post and delete methods

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const express = require("express");
 const cors = require("cors");
 const constants = require("./constants");
+const mongoose = require("mongoose");
+
+const calculationSchema = new mongoose.Schema({
+  calculation: String,
+  answer: Number,
+});
+const Calculation = mongoose.model("Calculation", calculationSchema);
+
+const DBConnection = async () => {
+  await mongoose.connect("mongodb://localhost:27017/wakeup-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  console.log("DB connected...");
+};
+
+DBConnection();
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // for parsing application/json
 app.listen(443, () => {
   console.log("App listening at 'http://localhost:443/'");
 });
 
 app.get("/calculations", (req, res) => {
-  res.send({
-    calculations: constants.SIMPLE_CALCULATIONS,
-    answers: constants.SIMPLE_CALCULATIONS_ANSWERS,
+  Calculation.find({}, (err, calculations) => {
+    var calculationMap = {};
+
+    calculations.forEach((calculation) => {
+      calculationMap[calculation._id] = calculation;
+    });
+
+    res.send(calculationMap);
+  });
+});
+
+app.post("/calculations", (req, res) => {
+  const calc = new Calculation({
+    calculation: req.body.calculation,
+    answer: req.body.answer,
+  });
+  calc.save((err, newCalculation) => {
+    if (err) console.log(err);
+    res.send(newCalculation);
+  });
+});
+
+app.delete("/calculations/:calculationId", (req, res) => {
+  Calculation.findByIdAndRemove(req.params.calculationId, (err) => {
+    console.log(err);
+    res.send(req.params.calculationId);
   });
 });
